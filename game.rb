74c501719be76bf31e720e38462e1e62f8a48716
Geison Biazus <geisonbiazus@gmail.com>
2013-08-27 09:17:32 -0300

    added number of tries

--- game.rb
+++ game.rb
@@ -24,13 +24,15 @@ class Game
 			place = Case::PLACES[hist[:question][2] - 1]
 			message << "The detective asks witness if the victim was murdered by #{suspect} with #{weapon} in #{place}\n"
 			if hist[:answer] != 0 
-				message << "The witness answered that the murderer was not in #{place}\n" if hist[:answer] == 3
-				message << "The witness answered that the murderer was not with #{weapon}\n" if hist[:answer] == 2
+				message << "The witness answered that the murder was not in #{place}\n" if hist[:answer] == 3
+				message << "The witness answered that the murder was not with #{weapon}\n" if hist[:answer] == 2
 				message << "The witness answered that the victim wasn't murdered by #{suspect}\n" if hist[:answer] == 1
 			else
 				message << "The witness answered that it was right\n" 
-				message << "#{suspect} killed the victim with #{weapon} in #{place}" 
+				message << "#{suspect} killed the victim with #{weapon} in #{place}\n" 
+				message << "The detective resolved the case in #{@detective.history.length} tries"
 			end
+
 			
 		end
 			puts message
--- game_spec.rb
+++ game_spec.rb
@@ -10,46 +10,50 @@ describe Game do
 	end
 
 	it "should initialize and resolve the case" do
+		Game.any_instance.stub(:puts)
 		@game = Game.new
 		Detective.any_instance.should_receive(:investigate).and_call_original
 		@game.resolve
 	end	
 
-	it "should print the conversation 1" do
+	it "should print the conversation and say that the place is wrong" do
 		my_history = [{:question => [1, 1, 1], :answer => 3}, {:question => [1, 1, 4], :answer => 0}]
 		@game = Game.new
-		Detective.any_instance.should_receive(:history).and_return(my_history)
+		Detective.any_instance.should_receive(:history).twice.and_return(my_history)
 		message = "The detective asks witness if the victim was murdered by Charles B. Abbage with Peixeira in Redmond\n"
-		message << "The witness answered that the murderer was not in Redmond\n"
+		message << "The witness answered that the murder was not in Redmond\n"
 		message << "The detective asks witness if the victim was murdered by Charles B. Abbage with Peixeira in Tokio\n"
 		message << "The witness answered that it was right\n"
-		message << "Charles B. Abbage killed the victim with Peixeira in Tokio"
+		message << "Charles B. Abbage killed the victim with Peixeira in Tokio\n"
+		message << "The detective resolved the case in 2 tries"
 		Game.any_instance.should_receive(:puts).with(message)		
 		@game.resolve
 	end	
 
-	it "should print the conversation 2" do
+	it "should print the conversation and say that the weapon is wrong" do
 		my_history = [{:question => [1, 1, 1], :answer => 2}, {:question => [1, 1, 4], :answer => 0}]
 		@game = Game.new
-		Detective.any_instance.should_receive(:history).and_return(my_history)
+		Detective.any_instance.should_receive(:history).twice.and_return(my_history)
 		message = "The detective asks witness if the victim was murdered by Charles B. Abbage with Peixeira in Redmond\n"
-		message << "The witness answered that the murderer was not with Peixeira\n"
+		message << "The witness answered that the murder was not with Peixeira\n"
 		message << "The detective asks witness if the victim was murdered by Charles B. Abbage with Peixeira in Tokio\n"
 		message << "The witness answered that it was right\n"
-		message << "Charles B. Abbage killed the victim with Peixeira in Tokio"
+		message << "Charles B. Abbage killed the victim with Peixeira in Tokio\n"
+		message << "The detective resolved the case in 2 tries"
 		Game.any_instance.should_receive(:puts).with(message)		
 		@game.resolve
 	end	
 	
-	it "should print the conversation 3" do
+	it "should print the conversation and say that the guilty is wrong" do
 		my_history = [{:question => [1, 1, 1], :answer => 1}, {:question => [1, 1, 4], :answer => 0}]
 		@game = Game.new
-		Detective.any_instance.should_receive(:history).and_return(my_history)
+		Detective.any_instance.should_receive(:history).twice.and_return(my_history)
 		message = "The detective asks witness if the victim was murdered by Charles B. Abbage with Peixeira in Redmond\n"
 		message << "The witness answered that the victim wasn't murdered by Charles B. Abbage\n"
 		message << "The detective asks witness if the victim was murdered by Charles B. Abbage with Peixeira in Tokio\n"
 		message << "The witness answered that it was right\n"
-		message << "Charles B. Abbage killed the victim with Peixeira in Tokio"
+		message << "Charles B. Abbage killed the victim with Peixeira in Tokio\n"
+		message << "The detective resolved the case in 2 tries"
 		Game.any_instance.should_receive(:puts).with(message)		
 		@game.resolve
 	end
